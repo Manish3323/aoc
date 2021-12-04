c3e03b5f7a35e94b107cf62c5c366fda6c970e91
manish <gowardipemanish95@gmail.com>
2021-12-04 22:01:52 +0530

    day 4 some refactoring

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -5,66 +5,70 @@ import scala.::
 import scala.io.Source
 
 @main
-def Day4() =  {
+def Day4(): Unit =  {
   val input = IO.readLines("4.txt").toList
   val randomNumbers = input.head.split(",").map(_.toInt)
-  val grids = input.tail.sliding(6, 6).map(x => x.map(y => y.split(" ").filter(x => x != ""))).toList
+
+  def splitAndRemoveEmptyChars(string: String): Array[String] = {
+    string.split(" ").filter(x => x != "")
+  }
+
+  val grids = input.tail.sliding(6, 6).map(snapOf6lines => snapOf6lines.map(splitAndRemoveEmptyChars)).toList
   val unmarkedGrids = grids.map(_.map(x => x.map(n => (n.toInt, false)).toList)).map(_.filter(x => x.nonEmpty)).map(g => (g, false)).zipWithIndex
 
   var _markedGrids = unmarkedGrids
 
-  var allGridsWon: List[(List[List[(Int, Boolean)]], Boolean)] = List.empty
-  var allGridsWonList: Set[ Int] = Set.empty
-  var lastRandomN = Int.MinValue
-
-  def markGridColumn(grid: (List[List[(Int, Boolean)]], Boolean), num: Int) = {
-    val value = grid._1.transpose.map(row => {
+  def markElementsIfNumberFound(grid: List[List[(Int, Boolean)]], num: Int) =
+    grid.map(row => {
       row.map(n => if (n._1 == num) (n._1, true) else n)
     })
-    (value, value.exists(r => checkRowCompleted(r)))
-  }
 
-  def markGridRow(grid: (List[List[(Int, Boolean)]], Boolean), num: Int) = {
-    val value = grid._1.map(row => {
-      row.map(n => if (n._1 == num) (n._1, true) else n)
-    })
-    (value, value.exists(r => checkRowCompleted(r)))
-  }
+  def markGridColumn(grid: (List[List[(Int, Boolean)]], Boolean), num: Int) =
+    val _grid = markElementsIfNumberFound(grid._1.transpose, num)
+    (_grid, _grid.exists(row => checkRowCompleted(row)))
+
+
+  def markGridRow(grid: (List[List[(Int, Boolean)]], Boolean), num: Int) =
+    val _grid = markElementsIfNumberFound(grid._1, num)
+    (_grid, _grid.exists(r => checkRowCompleted(r)))
+
 
   def checkRowCompleted (row: List[(Int, Boolean)]): Boolean = {
     row.forall(n => n._2)
   }
 
-  def addGridToWinnerList(index: Int, grid: (List[List[(Int, Boolean)]], Boolean)): Unit = {
+  var allGridsWon: List[(List[List[(Int, Boolean)]], Boolean)] = List.empty
+  var allGridsWonList: Set[ Int] = Set.empty
+
+  def addGridToWinnerList(grid: (List[List[(Int, Boolean)]], Boolean), index: Int): Unit = {
     if (grid._2 && !allGridsWonList.contains(index)) {
       allGridsWon = allGridsWon :+ grid
       allGridsWonList = allGridsWonList ++ List(index)
     }
   }
 
-  def sumOfUnmarkedNumbersOfBoard(grid: List[List[(Int, Boolean)]]): Int = {
+  def sumOfUnmarkedNumbersOf(grid: List[List[(Int, Boolean)]]): Int = {
     grid.foldLeft(0)((sum1, r) => {
       sum1 + r.foldLeft(0)((sum, n) => if (!n._2) sum + n._1 else sum)
     })
   }
 
-  for {
+  val lastRandomNumber = for {
     randomNumber <- randomNumbers if allGridsWon.size < _markedGrids.length
-  } do {
-    _markedGrids = _markedGrids.map((g, index) => {
-      val row = markGridRow(g, randomNumber)
-      addGridToWinnerList(index, row)
-      val grid = markGridColumn(row, randomNumber)
-      addGridToWinnerList(index, grid)
-      lastRandomN = randomNumber
-      (grid, index)
+  } yield {
+    _markedGrids = _markedGrids.map((unmarkedGrid, index) => {
+      val rowsMarkedInGrid = markGridRow(unmarkedGrid, randomNumber)
+      addGridToWinnerList(rowsMarkedInGrid, index)
+      val columnsMarkedInGrid = markGridColumn(rowsMarkedInGrid, randomNumber)
+      addGridToWinnerList(columnsMarkedInGrid, index)
+      (columnsMarkedInGrid, index)
     })
+    randomNumber
   }
-  if(allGridsWon.size == _markedGrids.length) {
-    val sum = sumOfUnmarkedNumbersOfBoard(allGridsWon.last._1)
-    println(" sum : " + sum )
-    println(" lastRandom : " + lastRandomN)
-    println(" answer : " + sum * lastRandomN)
-  }
+
+  val sum = sumOfUnmarkedNumbersOf(allGridsWon.last._1)
+  println(" sum : " + sum)
+  println(" lastRandom : " + lastRandomNumber.last)
+  println(" answer : " + sum * lastRandomNumber.last)
 
 }
